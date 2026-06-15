e16ce37e97017d73114f04d9e0c27e87a88a356a
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-06-15 13:14:50 -0400

    Changing version number to something valid

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-transfer',
-    version='0.11.debugWAG1224-1',
+    version='0.11.post2.dev1',
     description="Content transfer for Wagtail",
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',
