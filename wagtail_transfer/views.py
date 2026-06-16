325fb8bedabea93d6622f1bb5858847f04f33a8e
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-06-16 09:16:10 -0400

    Attempt at preventing an instance of an object from being used in a call to get_uid_for_local_id().

--- wagtail_transfer/views.py
+++ wagtail_transfer/views.py
@@ -146,10 +146,11 @@ def objects_for_export(request):
 
     mappings = []
     for model, pk in object_references:
-        uid = get_locator_for_model(model).get_uid_for_local_id(pk)
-        mappings.append(
-            [model._meta.label_lower, pk, uid]
-        )
+        if not isinstance(model):
+            uid = get_locator_for_model(model).get_uid_for_local_id(pk)
+            mappings.append(
+                [model._meta.label_lower, pk, uid]
+            )
 
     return JsonResponse({
         'ids_for_import': [],
